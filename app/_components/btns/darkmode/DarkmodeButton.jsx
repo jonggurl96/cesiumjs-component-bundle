5d7570ead8f46a3f5a0e82de29c3935342b2e860
jonggurl <jonggurl96@naver.com>
2025-06-16 16:09:24 +0900

    Button 기능 추가

---
+++ app/_components/btns/darkmode/DarkmodeButton.jsx
@@ -0,0 +1,13 @@
+import Button from "@components/btns/(default)/Button.jsx";
+import React  from 'react';
+
+export default function DarkmodeButton() {
+	return (
+		<Button style={{
+			width : 40,
+			height: 40,
+		}}>
+			버튼
+		</Button>
+	);
+}
--- components/btns/(default)/Button.jsx
+++ components/btns/(default)/Button.jsx
@@ -3,11 +3,20 @@ import styles from './Button.module.css';
 
 /**
  * Default Button
+ * @param style {React.CSSProperties?}
+ * @param onClick {function(MouseEvent?)}
+ * @param children
  * @returns {JSX.Element}
  * @constructor
  */
-export default function Button() {
+export default function Button({ style, onClick, children }) {
 	return (
-		<div className={styles.Button}></div>
+		<div
+			className={styles.Button}
+			style={style || {}}
+			onClick={onClick}
+		>
+			{children}
+		</div>
 	);
 }
