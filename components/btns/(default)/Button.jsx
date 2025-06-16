f104f0cc2e706710ebc8f442aba842257696f9be
jonggurl <jonggurl96@naver.com>
2025-06-16 14:47:35 +0900

    Button 추가

---
+++ components/btns/(default)/Button.jsx
@@ -0,0 +1,13 @@
+import React  from 'react';
+import styles from './Button.module.css';
+
+/**
+ * Default Button
+ * @returns {JSX.Element}
+ * @constructor
+ */
+export default function Button() {
+	return (
+		<div className={styles.Button}></div>
+	);
+}
